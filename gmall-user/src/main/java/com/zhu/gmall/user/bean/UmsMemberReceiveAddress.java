e5fc6adcdab6a9049b69fd597ed106a68724cf0d
zhuxuhua <1013905578>
2020-02-25 14:06:58 +0800

    get_address

---
+++ gmall-user/src/main/java/com/zhu/gmall/user/bean/UmsMemberReceiveAddress.java
@@ -0,0 +1,113 @@
+package com.zhu.gmall.user.bean;
+
+import javax.persistence.Id;
+
+public class UmsMemberReceiveAddress {
+    @Id
+    private String id;
+    private String memberId;
+    private String  name;
+    private String  phoneNumber;
+    private int defaultStatus;
+    private String postCode;
+    private String province;
+    private String city;
+    private String region;
+    private String detailAddress;
+
+    @Override
+    public String toString() {
+        return "UmsMemberReceiveAddress{" +
+                "id='" + id + '\'' +
+                ", memberId='" + memberId + '\'' +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", defaultStatus=" + defaultStatus +
+                ", postCode='" + postCode + '\'' +
+                ", province='" + province + '\'' +
+                ", city='" + city + '\'' +
+                ", region='" + region + '\'' +
+                ", detailAddress='" + detailAddress + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getDefaultStatus() {
+        return defaultStatus;
+    }
+
+    public void setDefaultStatus(int defaultStatus) {
+        this.defaultStatus = defaultStatus;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDetailAddress() {
+        return detailAddress;
+    }
+
+    public void setDetailAddress(String detailAddress) {
+        this.detailAddress = detailAddress;
+    }
+}
---
+++ gmall-user/src/main/java/com/zhu/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,9 @@
+package com.zhu.gmall.user.mapper;
+
+import com.zhu.gmall.user.bean.UmsMemberReceiveAddress;
+import org.springframework.stereotype.Component;
+import tk.mybatis.mapper.common.Mapper;
+
+@Component("umsMemberReceiveAddress")
+public interface UmsMemberReceiveAddressMapper  extends Mapper<UmsMemberReceiveAddress> {
+}
